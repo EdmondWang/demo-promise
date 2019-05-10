bbf46eadf9e27f42018c394bbe2dfae3dd8a1e5c
Edmond Wang <ywang53@ebay.com>
2019-05-10 15:40:19 +0800

    add error-handle

---
+++ error-handle.js
@@ -0,0 +1,11 @@
+let p = new Promise((resolve, reject) => {
+        resolve();
+    })
+    .then(() => {
+        setTimeout(() => {
+            throw new Error();
+        });
+    })
+    .catch(err => {
+        console.log("catch");
+    });
